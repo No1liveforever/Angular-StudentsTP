dd7bfdf8ab3b236f4df377d1220b2e1014547740
No1liveforever <begat651>
2025-01-25 16:05:13 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Personne } from './classes/personne';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TPTestAngular1';
+  personne:Personne = new Personne(10,'khalid',"abdllah");
+  tab: number[]= [12,13,17,90];
+  public val: number=10;
+  direBonjour():string{
+    return 'Bonjour les amis';
+  }
+
+  sumfunction(nb1,nb2): number{
+    return nb1+ nb2;
+  }
+
+  public AfficherAlert(): void{
+    alert('Hi khalid :)')
+  }
+
+  public text:string='';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { HeaderComponent } from './header/header.component';
+import { ImageComponent } from './image/image.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page1Component,
+    HeaderComponent,
+    ImageComponent,
+    InfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule // Add FormsModule here
   ],
   providers: [],
   bootstrap: [AppComponent]
